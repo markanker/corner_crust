5c5de53d127998f7c6ef0c4f9a815a27eada1953
afreeone <7lmarklibriets@gmail.com>
2025-06-13 12:53:15 +0300

    made user models, now going to make others

--- corner_crust/users/models.py
+++ corner_crust/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone_number = models.TextField(max_length=20, unique=True, null=True, blank=True)
